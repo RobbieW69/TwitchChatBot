e85b5c0f83182516c3f6161b02d92370b2ec4f20
Robbie W <running_bear1@hotmail.com>
2017-12-02 22:24:10 -0600

    Update TwitchChatBotMain.cs

--- TwitchChatBotMain.cs
+++ TwitchChatBotMain.cs
@@ -1,4 +1,4 @@
-﻿//A big thankyou goes out to HardlyDifficult
+//A big thankyou goes out to HardlyDifficult
 //Go watch all of his youtube videos @https://www.youtube.com/channel/UC3bHnBF2Q-u-1NEYG0Xwgeg
 //Signing off, RobbieW.
 using System;
@@ -24,9 +24,9 @@ namespace TwitchChatBot
         /// 3.Make a scroll wheel for client
         /// 4.Send a message every 10 minutes
         /// 5.Remember timeout 'default=600, max=1209600'
-        /// 6.GET THE USERS DAMN IT WE NEED THAT
+        /// 6.I HAVE AN IDEAGHJFYJTRDR6YD6RYD5(users)
         /// 7.See if there is a way to log a chat side ban
-        /// 8.Find all of the TwitchCmds ex:swriter.WriteLine($"JOIN #{cName}"); "JOIN"
+        /// 8.Find all of the TwitchCmds ex:swriter.WriteLine($"JOIN #{cName}"); "JOIN" %
         /// 9.Periodically host different streamers, every 30 minutes.
         /// 10.Add ALL commands to bot side. ALLLLL.
         /// 11.Add other stuff for cmds i.e. Hardware cmds
@@ -39,9 +39,7 @@ namespace TwitchChatBot
 
         //uname is short for Username, I'm lazy.
         string uname = "robbiew_yt", cName = "robbiew_yt",
-            pwd = "oauth:", chatInfo; //Client Info
-
-        string senderr; //Gets set to senders user name
+            pwd = "oauth:rpxjvmttul6gjuqzwbncvy3l1soltj", chatInfo; //Client Info
 
         string[] cmds = {"Use !help or !cmds or !commands to get a list of all the commands available.\n",
         "!sens\n", "!shutdown\n"}; //Dunno if I'll keep this or not yet, we'll see, might be easier with List?
@@ -62,6 +60,10 @@ namespace TwitchChatBot
         private void Form1_Load(object sender, EventArgs e)
         {
             #region TwitchBotStuff
+            //We can't put Connect(); here because Form1.Load is only called once
+            //When the application initally starts.
+            //"But we only need to connect once?" Yes but what if we get disconnected somehow?
+            //It's better to leave it as its own function in case we need it.
             if (tcpClient.Connected)
             {
                 //If we're connected to chat, Initialize the streams.
@@ -77,9 +79,9 @@ namespace TwitchChatBot
                 //Let's join my chat now
                 swriter.WriteLine($"JOIN #{cName}");
                 swriter.WriteLine("CAP REQ :twitch.tv/membership");
-                swriter.WriteLine("CAP REQ :twitch.tv/tags");
-                swriter.WriteLine("CAP REQ :twitch.tv/commands");
-                swriter.WriteLine($"CAP REQ :twitch.tv/NAMES");
+                //swriter.WriteLine("CAP REQ :twitch.tv/tags");
+                //swriter.WriteLine("CAP REQ :twitch.tv/commands");
+                //swriter.WriteLine($"CAP REQ :twitch.tv/NAMES");
                 swriter.Flush();
             }
             else
@@ -116,20 +118,42 @@ namespace TwitchChatBot
                 //So we need to split it up
                 if (msg.Contains("PRIVMSG"))//If a user sent a msg in chat
                 {
-                    string[] msg1 = msg.Split('!');
+                    //There are 4 different ways to approch this
+
+                    //Method 1
+                  /*string[] msg1 = msg.Split('!');
                     string msg2 = msg1[0]; // ':uname'! <-
                     string[] msgMsg = msg.Split(':');
                     string msg3 = msgMsg[2];  // :uname.......:'msg' <-
                     senderr = msg2.Remove(0, 1);//Takes away the ":" before the uname
-                    clabel.Text += $"\r\n{msg2.Remove(0,1)}:{msg3}"; //Uname:Msg :D
-                    if (msg3.StartsWith("!"))//If the message is a command
+                    clabel.Text += $"\r\n{msg2}:{msg3}";//Display message*/
+
+                    //Method 2
+                    string str = msg.Split(':')[2]; //Message
+                    string msgg = msg.Split('!')[0].TrimStart(':');//Username
+                    clabel.Text += $"\r\n{msgg.Insert(msgg.Length, ":").Insert(msgg.Length + 1, str)}";//Display message
+
+                    //Method 3
+                  /*string pls = msg.Split('!')[0].TrimStart(':').Insert(msg.Split('!')[0].TrimStart(':').Length, ":").Insert(msg.Split('!')[0].TrimStart(':').Length + 1, msg.Split(':')[2]);
+                    clabel.Text += $"\r\n{pls}";*/
+
+                    
+                    //Method 4
+                    //clabel.Text += $"\r\n{msg.Split('!')[0].TrimStart(':').Insert(msg.Split('!')[0].TrimStart(':').Length, ":").Insert(msg.Split('!')[0].TrimStart(':').Length + 1, msg.Split(':')[2])}";
+
+                    //In my own personal opinion, Method 2 is the most effcient, both for reading and processing.
+                    //Its short, sweet, to the point and leaves no questions.
+                    //Though any method will work and there are more than just 4
+
+
+                    if (str.StartsWith("!"))//If the message is a command
                     {
-                        Commands(msg3, senderr); //Method to do cmds
+                        Commands(str, msgg); //Process Commands
                     }
                 }
                 else
                 {
-                    clabel.Text += $"\n{msg}";
+                    clabel.Text += $"\n{msg}";//All of the "Joined" messages from Twitch.
                 }
             }
         }
@@ -141,6 +165,9 @@ namespace TwitchChatBot
         /// <param name="se">Sender</param>
         private void Commands(string cmd, string sender)
         {
+            List<string> votes = new List<string>();
+            int votez = 0;
+            string banee;
             if (cmd == "!help" || cmd == "!cmds" || cmd == "!commands")
                 SendMessage(cmds);
             if (cmd == "!sens")
@@ -180,15 +207,6 @@ namespace TwitchChatBot
                     SendMessage("Banlist: Vacant");
                 }
             }
-            if (cmd.StartsWith("!to"))//Timeout user
-            {
-                //Still need to implement [1]User and [2]Time
-                //Lets limit it to 10 minutes for now.
-                string[] ban = cmd.Split(' ');
-                string user = ban[1];
-                SendMessage($"/timeout {user}");
-                SendMessage(user + " was timed out by a mod.");
-            }
             if (cmd.StartsWith("!vote"))
             {
                 //Doing a !vote_type kind of command
@@ -196,35 +214,27 @@ namespace TwitchChatBot
                 //ex2:!vote timeout robbie 20seconds
                 //ex3:!vote game overwatch
                 //ex4:!vote clear (to clear chat)
-                //Maybe use switches? Idk, still hard enough getting the ban to count
-                List<string> senders = new List<string>();//Senders
-                Dictionary<string, int> vBans = new Dictionary<string, int>();//User/int
-                Dictionary<string, int> vTos = new Dictionary<string, int>();
                 string[] vote = cmd.Split(' ');
+                banee = vote[2];
                 string type = vote[1]; //Type of vote
                 if (type == "ban")//" ", !vote = 0, ban = 1, name = 2|| !vote ban name
                 {
-                    string ban = vote[2]; 
-                    if (senders.Contains(sender))
+                    clabel.Text += "\r\n\nsender = " + sender + "\n";
+                    if(!votes.Contains(sender))
                     {
-                        SendMessage("You already voted on this ban, " + sender);
+                        clabel.Text += "votes doesnt have sender";
+                        votes.Add(sender); //Add sender to a list
+                        clabel.Text += sender + " has been added to list.";
+                        votez++;
+                        SendMessage(sender, " thankyou for your vote.");
+                        clabel.Text += banee + " has " + votez + " to be banned.";
                     }
-                    else 
+                    else if (votes.Contains(sender))
                     {
-                        senders.Add(sender); //Added sender
-                        if (!vBans.ContainsKey(ban))
-                        {
-                            SendMessage("Vote added, " + sender);
-                            vBans.Add(ban, 1); //Added user to be banned + num
-                        }
-                        else
-                        {
-                            vBans[ban]++;
-                            SendMessage("A vote on " + ban + " has already been started, " + sender);
-                            SendMessage("Total votes for " + ban + " are:" + vBans[ban]);
-                        }
+                        clabel.Text += "votes has sender";
+                        clabel.Text += sender + " tried to re vote";
+                        SendMessage(sender + ", You have already voted to ban " + banee);
                     }
-
                 }
                 if (type == "clear")
                 {
@@ -240,7 +250,17 @@ namespace TwitchChatBot
                     string str = gameWanted.ToLower();//game name to go in dict
                 }
             }
-
+            /*if (cmd == "!votes")
+            {
+                if (votez <= 0)
+                {
+                    SendMessage("There is no vote currently.");
+                }
+                else
+                {
+                    SendMessage(banee + " has " + votez + " votes.");
+                }
+            }*/
         }
 
         /// <summary>
